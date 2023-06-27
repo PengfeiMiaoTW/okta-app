7e94b17b11dd079a9dd21fc06080995bc85b4ad7
pengfei.miao <pengfei.miao@thoughtworks.com>
2023-06-27 21:37:22 +0800

    modify log level

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,5 +1,6 @@
 class HelloController < ApplicationController
   def empty
+    Rails.logger.debug "Request: #{request.method} #{request.fullpath}"
     render json: { message: "Hello, world!" }
   end
 
